f49f45067faba76ba413ea0092bb9dc4451e99ea
Christian Falardeau <christian.falardeau@bimone.com>
2021-07-26 11:48:56 -0400

    initial commit

---
+++ 2021/Source/Revit.IFC.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+#if IFC_OPENSOURCE
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IFC Common for Revit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Autodesk, Inc.")]
+[assembly: AssemblyProduct("IFC Import/Exporter for Revit")]
+[assembly: AssemblyCopyright("© 2012-2021 Autodesk, Inc.All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("21.3.0.0")]
+[assembly: AssemblyFileVersion("21.3.0.0")]
+#endif
+
+
+// Version information can now be found in Source\Foundation\RevitENU\Version.cs
+//
+
