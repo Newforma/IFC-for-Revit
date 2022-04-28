7e4361356796f5cd97b9aad07c6ba5b8148d2764
Yannick Bolduc <yannick.bolduc@bimone.com>
2022-04-28 13:53:03 -0400

    Use master branch of Autodesk/revit-ifc to create 2023 version. https://github.com/Autodesk/revit-ifc/commit/df1485b9accd598c2912a055af205ee1b03648c7

---
+++ 2023/Source/Revit.IFC.Export/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+#region Using directives
+
+#if IFC_OPENSOURCE
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IFC Exporter for Revit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Autodesk, Inc.")]
+[assembly: AssemblyProduct("IFC Exporter for Revit")]
+[assembly: AssemblyCopyright("� 2012-2022 Autodesk, Inc. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("23.1.1.0")]
+[assembly: AssemblyFileVersion("23.1.1.0")]
+
+#endif
+
+
+// Version information can now be found in Source\Foundation\RevitENU\Version.cs
+//
+#endregion
+
