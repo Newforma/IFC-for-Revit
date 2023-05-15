c028c142d158edc618ee685b67949283986cc1b8
MikeBedardBimOne <132077824+MikeBedardBimOne@users.noreply.github.com>
2023-05-15 15:22:24 -0400

    Use master branch of Autodesk/revit-ifc to create 2024 version (#18)

---
+++ 2024/Source/Revit.IFC.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+#if IFC_OPENSOURCE
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IFC Common for Revit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Autodesk, Inc.")]
+[assembly: AssemblyProduct("IFC Import/Exporter for Revit")]
+[assembly: AssemblyCopyright("© 2012-2022 Autodesk, Inc.All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("24.0.6.0")]
+[assembly: AssemblyFileVersion("24.0.6.0")]
+#endif
+
+
+// Version information can now be found in Source\Foundation\RevitENU\Version.cs
+//
+
