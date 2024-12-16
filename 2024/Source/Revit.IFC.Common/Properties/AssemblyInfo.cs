51fae55aa9a70ff0c77a72d13ad1f11837ca45b0
Simon Lemieux <simon.lemieux@bimone.com>
2024-12-16 09:29:02 -0500

    Update IFC.Common and IFC.Export librairies to the latest version for Revit 2024

--- 2024/Source/Revit.IFC.Common/Properties/AssemblyInfo.cs
+++ 2024/Source/Revit.IFC.Common/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 #if IFC_OPENSOURCE
 using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IFC Common for Revit")]
@@ -9,12 +9,12 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Autodesk")]
 [assembly: AssemblyProduct("IFC Import/Exporter for Revit")]
-[assembly: AssemblyCopyright("� 2012-2024 Autodesk, Inc.All rights reserved.")]
+[assembly: AssemblyCopyright("� 2012-2023 Autodesk, Inc.All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("24.2.1.49")]
-[assembly: AssemblyFileVersion("24.2.1.49")]
+[assembly: AssemblyVersion("24.2.20.1")]
+[assembly: AssemblyFileVersion("24.2.20.1")]
 #endif
 
 
--- 2024/Source/Revit.IFC.Export/Properties/AssemblyInfo.cs
+++ 2024/Source/Revit.IFC.Export/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 #region Using directives
 
 #if IFC_OPENSOURCE
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IFC Exporter for Revit")]
@@ -10,12 +10,12 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Autodesk")]
 [assembly: AssemblyProduct("IFC Exporter for Revit")]
-[assembly: AssemblyCopyright("� 2012-2024 Autodesk, Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("� 2012-2023 Autodesk, Inc. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("24.2.1.49")]
-[assembly: AssemblyFileVersion("24.2.1.49")]
+[assembly: AssemblyVersion("24.2.20.1")]
+[assembly: AssemblyFileVersion("24.2.20.1")]
 
 #endif
 
